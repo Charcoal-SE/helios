a115d6bb781e8c3ac9b7c3838ac08d0842dec94a
AWegnerGitHub <andy@andrewwegner.com>
2018-01-03 13:41:25 -0600

    Return error message if incomplete endpoint is hit. Refs #4

---
+++ blacklists/blacklist_errors.py
@@ -0,0 +1,19 @@
+import logging
+import json
+
+
+def create_blacklist_item(event, context):
+    """
+    Defines the error that is thrown if someone hits the naked blacklist/ endpoint
+    without an {id}
+    """
+    response = {
+        'statusCode': 404,
+        'body': json.dumps({
+            'items': [],
+            'num_items': 0,
+            'message': "Invalid endpoint. An {id} must be supplied.",
+            'error_type': "invalid_endpoint"
+        })
+    }
+    return response
